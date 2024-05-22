8e6421ffd17abc10d4bbf9e357fb4bed213e6976
Balaguer Senabre <lbalaguer@minsait.com>
2024-05-22 12:50:01 +0200

    Fin de la sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/components/counter.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeroComponent, ListComponent],
+  imports: [BrowserModule, AppRoutingModule, CounterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/counter/components/counter.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CounterComponent } from './counter.component';
+
+@NgModule({
+  declarations: [CounterComponent],
+  exports: [CounterComponent],
+  imports: [],
+  providers: [],
+})
+export class CounterModule {}
